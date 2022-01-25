37b984eb674ff9a456ad525c3b386e90e114e53c
Omar Ashour <omar.ashour22@gmail.com>
2022-01-25 11:16:13 -0800

    Add deps

--- src/PatternFormation.jl
+++ src/PatternFormation.jl
@@ -1,3 +1,15 @@
+using Logging: global_logger
+using TerminalLoggers: TerminalLogger
+global_logger(TerminalLogger())
+
+using DifferentialEquations, LinearAlgebra
+using SparseArrays, Symbolics
+using Plots
+using SparseDiffTools
+using AlgebraicMultigrid
+using LoopVectorization
+using StaticArrays
+using LightLattices
 module PatternFormation
 
 # Write your package code here.
