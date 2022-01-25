88d4a06834592c9c0386d5b470232fa6d59f439e
Documenter.jl <documenter@juliadocs.github.io>
2022-01-25 19:09:01 +0000

    build based on a022076

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = PatternFormation","category":"page"},{"location":"#PatternFormation","page":"Home","title":"PatternFormation","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for PatternFormation.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [PatternFormation]","category":"page"}]
+}
