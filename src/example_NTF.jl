2d5e8b787ad849258b5642f462fb8f92dad794fe
Omar Ashour <omar.ashour22@gmail.com>
2022-02-01 22:23:20 -0800

    Fix NTF

--- src/example_NTF.jl
+++ src/example_NTF.jl
@@ -23,8 +23,8 @@ Mᵤ = 1
 Mᵥ = 1
 γᵤ = 0.1
 γᵥ = 0.2
-a = 0.5*0
-c = 0.00125*0
+a = 0.5
+c = 0.00125
 N_threads = 1
 BLAS.set_num_threads(N_threads)
 ex = ThreadedEx(simd = true)
@@ -40,6 +40,8 @@ println("Hello")
 #u01 = 1 .- u01        
 u01, x, y, dx, dy = init_cond(:NoisePatches, N, c01 = 0.01)
 u02, x, y, dx, dy = init_cond(:NoisePatches, N, c01 = 0.25)
+#u01, x, y, dx, dy = init_cond(:ErMnO3_MnOnly, N, M = 11, α=3000, BC=:Periodic)
+#u02, x, y, dx, dy = init_cond(:ErMnO3_ErOnly, N, M = 11, α=500, BC=:Periodic)
 u0 = cat(u01, u02, dims=3)
 heatmap(x, y, u01')
 heatmap(x, y, u02')
@@ -49,17 +51,17 @@ func(du,u,p,t) = myEquation(du, u, p, t, ex)
 prob = ODEProblem(func, u0, tspan, p)
 
 # Solve!
-println("Solving")
-@time sol = solve(prob, ROCK2(), saveat=range(0, stop=tspan[2], length=101), progress=true, progress_steps=1)
-println("Done!")
+#println("Solving")
+#@time sol = solve(prob, ROCK2(), saveat=range(0, stop=tspan[2], length=101), progress=true, progress_steps=1)
+#println("Done!")
 
 # Plot!
-println("Plotting")
-anim = @animate for i in 1:length(sol.t)
-  tit = "$type-type; f = $f, k = $k; t = $(sol.t[i])" 
-  #u = kron(ones(3,3), sol.u[i][:,:,1])
-  u = sol.u[i][:,:,1]
-  heatmap(u, c=:grays, aspect_ratio=dx/dy, axis=([], false), title=tit, colorbar=false, clims=(minimum(u),maximum(u)))
-end
+#println("Plotting")
+#anim = @animate for i in 1:length(sol.t)
+#  tit = "$type-type; f = $f, k = $k; t = $(sol.t[i])" 
+#  #u = kron(ones(3,3), sol.u[i][:,:,1])
+#  u = sol.u[i][:,:,1]
+#  heatmap(u, c=:grays, aspect_ratio=dx/dy, axis=([], false), title=tit, colorbar=false, clims=(minimum(u),maximum(u)))
+#end
 
-gif(anim, fps=10)
+#gif(anim, fps=10)
