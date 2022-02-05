e9b1e11c7d816d348eb32dcb7c82a16b943f6ba0
Omar Ashour <omar.ashour22@gmail.com>
2022-02-05 09:39:02 -0800

    Add cubic nonlinearity

--- src/NTF_prototype.jl
+++ src/NTF_prototype.jl
@@ -7,34 +7,43 @@ global_logger(TerminalLogger())
 using OrdinaryDiffEq, LinearAlgebra, Plots, DiffEqOperators, SparseArrays, Distributions
 using Symbolics, SparseDiffTools
 using PreallocationTools
+using PatternFormation
+
+using LinearSolve
 
 BLAS.set_num_threads(1)
 
 # Generate the constants
 println("Setting up problem parameters")
-Mᵤ = 2e-5
+Mᵤ = 5*1e-5
 Mᵥ = 1e-5
-γᵤ = 1e-2
-γᵥ = 1e-2
-a = 0.5
-c = 0.00125
+γᵤ = 1e-5
+γᵥ = 1e-5
+a = 0.062 # F
+c = 0.061 # k
+type = "π"
 
-dx = 1/100
-dy = 1/100
-N = 10
-tspan = (0.0, 100.0)
+dx = 1/50
+dy = 1/50 # Can't be too small? Instability Detected
+N = 1000 
+tspan = (0.0, 1000.0)
 
 # IC
 println("Initial Conditions")
 r01 = 0.01 .+ rand(Uniform(-0.01/100, +0.01/100), N, N)
 r02 = 0.25 .+ rand(Uniform(-0.25/100, +0.25/100), N, N)
+#r01, x, y, dx, dy = init_cond(:NoisePatches, N, c01 = 0.01, dx = dx)
+#r02, x, y, dx, dy = init_cond(:NoisePatches, N, c01 = 0.25, dx = dx)
+#r01, x, y, dx, dy = init_cond(:ErMnO3_MnOnly, N, M = 9, α=3000, BC=:Neumann)
+#r02, x, y, dx, dy = init_cond(:ErMnO3_ErOnly, N, M = 9, α=500, BC=:Neumann)
+r01 .= 1 .- r01
 r0 = cat(r01, r02, dims=3)
 
 # Generate matrices and stuff
 println("Dealing with finite differencing.")
 # See https://www.ljll.math.upmc.fr/frey/ftp/finite-differences.pdf to compare to the general scheme
-Q = Neumann0BC(1.0, 1)
-#Q = PeriodicBC(Float64)
+#Q = Neumann0BC(1.0, 1)
+Q = PeriodicBC(Float64)
 D4 = CenteredDifference(4, 2, 1.0, N)
 D2 = CenteredDifference(2, 2, 1.0, N)
 D1 = CenteredDifference(1, 2, 1.0, N)
@@ -45,6 +54,10 @@ L1 = D1*Q
 A4 = sparse(L4)[1]
 A2 = sparse(L2)[1]
 A1 = sparse(L1)[1]
+
+#A2 = sparse(Tridiagonal([1.0 for i in 1:N-1],[-2.0 for i in 1:N],[1.0 for i in 1:N-1]))
+#A2[2,1] = 2.0
+#A2[end-1,end] = 2.0
 
 println("Dealing with caches.")
 A4u = DiffEqBase.dualcache(zeros(N,N))
@@ -69,23 +82,19 @@ D1v = DiffEqBase.dualcache(zeros(N,N))
 cache =  [A4u, uA4, D4u, A4v, vA4, D4v, A2u, uA2, D2u, A2v, vA2, D2v, A1u, uA1, D1u, A1v, vA1, D1v]
 p = [Mᵤ, Mᵥ, γᵤ, γᵥ, a, c, dx, dy, A1, A2, A4, cache]
 
-function basic_version!(dr,r,p,t)
-  Mᵤ, Mᵥ, γᵤ, γᵥ, a, c, dx, dy, A1, A2, A4 = p
-
-  u = @view r[:,:,1]
-  v = @view r[:,:,2]
-  D4u = -γᵤ*Mᵤ*(1/dy^4*A4*u + 1/dx^4*u*A4')
-  D4v = -γᵥ*Mᵥ*(1/dy^4*A4*v + 1/dx^4*v*A4')
-  D2u = 2Mᵤ*(6u.^2 .- 6u .+ 1).*(1/dy^2*A2*u + 1/dx^2*u*A2')
-  D2v = 2Mᵥ*(6v.^2 .- 6v .+ 1).*(1/dy^2*A2*v + 1/dx^2*v*A2')
-  D1u = 12Mᵤ*(2u .- 1).*(1/dy*A1*u + 1/dx*u*A1').^2
-  D1v = 12Mᵥ*(2v .- 1).*(1/dy*A1*v + 1/dx*v*A1').^2
-  dr[:,:,1] = D4u + D2u + D1u .+ c .- a*v*u 
-  dr[:,:,2] = D4v + D2v + D1v .+ c .- a*v*u
-end
+#function basic_version_GS!(dr,r,p,t)
+#  Mᵤ, Mᵥ, γᵤ, γᵥ, a, c, dx, dy, A1, A2, A4 = p
+#
+#  u = @view r[:,:,1]
+#  v = @view r[:,:,2]
+#  D2u = Mᵤ*(1/dy^2*A2*u + 1/dx^2*u*A2')
+#  D2v = Mᵥ*(1/dy^2*A2*v + 1/dx^2*v*A2')
+#  dr[:,:,1] = D2u .- u.*v.^2 + a*(1 .- u)
+#  dr[:,:,2] = D2v .+ u.*v.^2 - (a+c)*v
+#end
 
 function advanced_version!(dr,r,p,t)
-  Mᵤ, Mᵥ, γᵤ, γᵥ, a, c, dx, dy, A1, A2, A4, cache = p
+  Mᵤ, Mᵥ, γᵤ, γᵥ, F, k, dx, dy, A1, A2, A4, cache = p
   A4u, uA4, D4u, A4v, vA4, D4v, A2u, uA2, D2u, A2v, vA2, D2v, A1u, uA1, D1u, A1v, vA1, D1v = cache
 
   u = @view r[:,:,1]
@@ -124,45 +133,50 @@ function advanced_version!(dr,r,p,t)
   mul!(uA1,u,A1')
   mul!(A1v,A1,v)
   mul!(vA1,v,A1')
-  #BLAS.gemm!('N','N', 1.0, A4, u, 0.0, A4u)
-  #BLAS.gemm!('N','T', 1.0, u, A4, 0.0, uA4)
-  #BLAS.gemm!('N','N', 1.0, A4, v, 0.0, A4v)
-  #BLAS.gemm!('N','T', 1.0, v, A4, 0.0, vA4)
-  #BLAS.gemm!('N','N', 1.0, A2, u, 0.0, A2u)
-  #BLAS.gemm!('N','T', 1.0, u, A2, 0.0, uA2)
-  #BLAS.gemm!('N','N', 1.0, A2, v, 0.0, A2v)
-  #BLAS.gemm!('N','T', 1.0, v, A2, 0.0, vA2)
-  #BLAS.gemm!('N','N', 1.0, A1, u, 0.0, A1u)
-  #BLAS.gemm!('N','T', 1.0, u, A1, 0.0, uA1)
-  #BLAS.gemm!('N','N', 1.0, A1, v, 0.0, A1v)
-  #BLAS.gemm!('N','T', 1.0, v, A1, 0.0, vA1)
 
   @. D4u = -γᵤ*Mᵤ*(1/dy^4*A4u + 1/dx^4*uA4)
   @. D4v = -γᵥ*Mᵥ*(1/dy^4*A4v + 1/dx^4*vA4)
   @. D2u = 2Mᵤ*(6u.^2 .- 6u .+ 1).*(1/dy^2*A2u + 1/dx^2*uA2)
   @. D2v = 2Mᵥ*(6v.^2 .- 6v .+ 1).*(1/dy^2*A2v + 1/dx^2*vA2)
-  @. D1u = 12Mᵤ*(2u .- 1).*(1/dy*A1u + 1/dx*uA1)
-  @. D1v = 12Mᵥ*(2v .- 1).*(1/dy*A1v + 1/dx*vA1)
-  @. du = D4u + D2u + D1u + c - a*v*u 
-  @. dv = D4v + D2v + D1v + c - a*v*u
+  @. D1u = 12Mᵤ*(2u .- 1).*(1/dy*A1u + 1/dx*uA1).^2
+  @. D1v = 12Mᵥ*(2v .- 1).*(1/dy*A1v + 1/dx*vA1).^2
+  @. du = D4u + D2u + D1u + c - a*u*v*v + F*(1-u)
+  @. dv = D4v + D2v + D1v + c + a*u*v*v - (F+k)*v
+  nothing
+end
+
+function basic_version!(dr,r,p,t)
+  Mᵤ, Mᵥ, γᵤ, γᵥ, F, k, dx, dy, A1, A2, A4 = p
+
+  u = @view r[:,:,1]
+  v = @view r[:,:,2]
+  D4u = -γᵤ*Mᵤ*(1/dy^4*A4*u + 1/dx^4*u*A4')
+  D4v = -γᵥ*Mᵥ*(1/dy^4*A4*v + 1/dx^4*v*A4')
+  D2u = 2Mᵤ*(6u.^2 .- 6u .+ 1).*(1/dy^2*A2*u + 1/dx^2*u*A2')
+  D2v = 2Mᵥ*(6v.^2 .- 6v .+ 1).*(1/dy^2*A2*v + 1/dx^2*v*A2')
+  D1u = 12Mᵤ*(2u .- 1).*(1/dy*A1*u + 1/dx*u*A1').^2
+  D1v = 12Mᵥ*(2v .- 1).*(1/dy*A1*v + 1/dx*v*A1').^2
+  dr[:,:,1] = D4u .+ D2u .+ D1u .- u.*v.^2 + F*(1 .- u)
+  dr[:,:,2] = D4v .+ D2v .+ D1v .+ u.*v.^2 - (F+k)*v
+  nothing
 end
 
 # Jacobian stuff
 println("Jacobian sparsity")
-dr0 = copy(r0)
-jac_sparsity = Symbolics.jacobian_sparsity((dr,r)->basic_version!(dr,r,p,0.0),dr0,r0)
-#f = ODEFunction(advanced_version!;jac_prototype=float.(jac_sparsity))
+#dr0 = copy(r0)
+#@time jac_sparsity = Symbolics.jacobian_sparsity((dr,r)->basic_version!(dr,r,p,0.0),dr0,r0)
+f = ODEFunction(advanced_version!;jac_prototype=float.(jac_sparsity))
 
-#println("Solving")
-#prob = ODEProblem(f,r0,tspan,p)
-#@time sol = solve(prob, Tsit5(), saveat=range(0.0, stop=tspan[2], length=101), progress=true, progress_steps=1)
-#
-#println("Plotting")
-#anim = @animate for i in 1:length(sol.t)
-#  tit = "t = $(sol.t[i])" 
-#  #u = kron(ones(3,3), sol.u[i][:,:,1])
-#  u = sol.u[i][:,:,1]
-#  heatmap(u, c=:grays, aspect_ratio=dx/dy, axis=([], false), title=tit, colorbar=false, clims=(minimum(u),maximum(u)))
-#end
-#
-#gif(anim, fps=10)
+println("Solving")
+prob = ODEProblem(f,r0,tspan,p)
+@time sol = solve(prob, ROCK2(), saveat=range(0.0, stop=tspan[2], length=101), progress=true, progress_steps=1)
+
+println("Plotting")
+anim = @animate for i in 1:length(sol.t)
+  title = "$type-type; f = $a, k = $c; t = $(sol.t[i])" 
+  #u = kron(ones(3,3), sol.u[i][:,:,1])
+  u = sol.u[i][:,:,2]
+  heatmap(u, c=:berlin, aspect_ratio=dx/dy, axis=([], false), title=title, colorbar=false)
+end
+
+gif(anim, fps=10)
